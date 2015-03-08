4089c4f929420bcd63235f4c7e3a0ec32c9e0b02
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-08 00:34:38 -0800

    refactor code

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
-require('sinatra')
-require('sinatra/reloader')
-also_reload('lib/**/*.rb')
-require("./lib/places_youve_been")
+require 'sinatra'
+require 'sinatra/reloader'
+also_reload 'lib/**/*.rb'
+require "./lib/places_youve_been"
 
-get('/') do
+get '/' do
   @places = Places.all()
   erb(:index)
 end
 
-post('/places_youve_been') do
+post '/places_youve_been' do
   locale = params.fetch("locale")
   place = Places.new(locale)
   place.save()
--- spec/places_youve_been_spec.rb
+++ spec/places_youve_been_spec.rb
@@ -7,20 +7,20 @@ describe Places do
   end
 
   describe "#locale" do
-    it("lets you enter a place you've been") do
+    it "lets you enter a place you've been" do
       test_place = Places.new("Canada")
       expect(test_place.locale()).to eq("Canada")
     end
   end
 
   describe ".all" do
-    it("is empty at first") do
+    it "is empty at first" do
       expect(Places.all()).to eq([])
     end
   end
 
   describe "#save" do
-    it("adds a place to the list of saved places") do
+    it "adds a place to the list of saved places" do
       test_place = Places.new("Nepal")
       test_place.save()
       expect(Places.all()).to eq([test_place])
